56f5c09a4d8e4f5e5cb8e0dc3f0dadf3e5e0c553
Jose A. Labanda <99726606+LVJose@users.noreply.github.com>
2022-12-18 20:07:10 +0100

    Comparador y máquina de estados completos

--- VHDL/vhdl_nexus_ddr4/Trabajo/Trabajo.srcs/sources_1/new/Comparator.vhd
+++ VHDL/vhdl_nexus_ddr4/Trabajo/Trabajo.srcs/sources_1/new/Comparator.vhd
@@ -3,96 +3,92 @@ use IEEE.STD_LOGIC_1164.all;
 use ieee.numeric_std.all;
 
 entity Comparator is
+   	generic(
+    PASS_ELEMENTS: positive := 3;     -- Number of elements in the password
+    INPUT_WIDTH: positive := 2;       -- Number of input's elements
+    OUTPUT_WIDTH: positive := 3       -- Number of elements needed for output
+    );
     port(
     RESET :         in std_logic;
-    INPUT_PASSWORD : in integer;
-    INPUT_ANSWER :  in integer;
-    RESULT1 :       out std_logic_vector (3 downto 0);   --Results to to FSM
-    RESULT2 :       out std_logic_vector (3 downto 0);
-    RESULT3 :       out std_logic_vector (3 downto 0);
-    RESULT4 :       out std_logic_vector (3 downto 0);
+    INPUT_PASSWORD : in std_logic_vector (INPUT_WIDTH downto 0);
+    INPUT_ANSWER :  in std_logic_vector (INPUT_WIDTH downto 0);
+    RESULT1 :       out std_logic_vector (OUTPUT_WIDTH downto 0);   --Results to to FSM
+    RESULT2 :       out std_logic_vector (OUTPUT_WIDTH downto 0);
+    RESULT3 :       out std_logic_vector (OUTPUT_WIDTH downto 0);
+    RESULT4 :       out std_logic_vector (OUTPUT_WIDTH downto 0);
     COMPLETE:       out std_logic;      --Array completed
     CORRECT:        out std_logic       --Correct response      
     );
 end Comparator;
 
 architecture Behavioral of Comparator is
-    type int_array is array (3 downto 0) of integer;
-    signal password : int_array := (others => 0);
-    signal answer : int_array := (others => 0);
-    signal complete_i : std_logic := '0';
-    signal correct_i : std_logic := '1';
-    type matrix is array (3 downto 0) of std_logic_vector (7 downto 0);
-    signal results : matrix := (others => ( others => '0'));
+    type matrix is array (PASS_ELEMENTS downto 0) of std_logic_vector (OUTPUT_WIDTH downto 0);
+    type matrix2 is array (PASS_ELEMENTS downto 0) of std_logic_vector (INPUT_WIDTH downto 0);
 begin
-    reset_input: process (RESET)
+    compare: process(INPUT_PASSWORD, INPUT_ANSWER, RESET)
+        variable counter: integer := 0;
+        variable password : matrix2  := (others => ( others => '0'));
+        variable answer : matrix2  := (others => ( others => '0'));
+        variable results : matrix := (others => ( others => '0'));
+        variable complete_i : std_logic := '0';
+        variable correct_a : std_logic := '1';    
     begin
-    	if RESET = '0' then
-        	password <= (others => 0);
-        	answer <= (others => 0);
-        end if;
-    end process;
-    
-    create_password: process(INPUT_PASSWORD)
-        begin
-        if INPUT_PASSWORD /= 0 then
-            if password(0) /= 0 then
-                    complete <= '1';
-            else    
-                for count in 3 downto 0 loop
-                    if password(count) = 0 then
-                        password(count) <= INPUT_PASSWORD;
+        if RESET = '0' then                                 -- Reset
+        	COMPLETE <= '0';
+        	CORRECT <= '0';
+            RESULT1 <= "1111";
+            RESULT2 <= "1111"; 
+            RESULT3 <= "1111"; 
+            RESULT4 <= "1111";
+        elsif RESET = '1' then
+            if INPUT_PASSWORD /= "000" and counter /= 0 then-- Receiving a password when it is uncompleted    
+                for count in PASS_ELEMENTS downto 0 loop                
+                    if password(count) = "000" then
+                        password(count) := INPUT_PASSWORD;  -- Places it in the last free slot of the array
+                        counter := count;
+                        exit;
                     end if;
                 end loop;
-            end if;
-        end if;
-    end process;
-    
-    create_answer: process(INPUT_ANSWER)
-        begin
-        if INPUT_ANSWER /= 0 then
-            if answer(0) /= 0 then
-                for count in 3 downto 0 loop
-                    if answer(count) = password(count) then
-                            results(count) <= "0000000";
-                    else
-                        correct <= '0';
-                        for count2 in 3 downto 0 loop
-                            if answer(count) = password(count2) then
-                                results(count) <= "0000001";
-                                exit;
-                            else
-                                results(count) <= "1111110";
-                            end if;
-                        end loop;
-                    end if;        
-                end loop;
-                complete <= '1';         
-            else    
-                for count in 3 downto 0 loop
-                    if answer(count) = 0 then
-                        answer(count) <= INPUT_ANSWER;
+                if counter = 0 then
+                    complete_i := '1';                      -- If all slots have been completed, sends a signal
+                end if;
+            elsif INPUT_ANSWER /= "000" then                -- Receiving an answer
+                for count in PASS_ELEMENTS downto 0 loop 
+                    if answer(count) = "000" then
+                        answer(count) := INPUT_ANSWER ;     -- Places it in the last free slot of the array
+                        counter := count;
+                        exit;
                     end if;
                 end loop;
+                if counter = 0 then                         -- Once the array is completed
+                    for count in PASS_ELEMENTS downto 0 loop    -- Checks all the slots
+                        if answer(count) = password(count) then
+                                results(count) := "1000";       -- If the answer is correct, sends the correct symbol
+                        else
+                            correct_a := '0';                   -- Entering here means the answer is not correct
+                            for count2 in PASS_ELEMENTS downto 0 loop
+                                if answer(count) = password(count2) then
+                                    results(count) := "0000";   -- The button is part of the password but it is not in place
+                                    exit;
+                                else
+                                    results(count) := "1111";   -- The button is not in the password
+                                end if;
+                            end loop;
+                        end if;        
+                    end loop;
+                    RESULT1 <= results(3);                       -- Overwrites the outputs   
+                    RESULT2 <= results(2); 
+                    RESULT3 <= results(1); 
+                    RESULT4 <= results(0);
+                    answer := (others => ( others => '0'));
+                    results := (others => (others => '0'));
+                    CORRECT <= correct_a;
+                    correct_a := '1';
+                    complete_i := '1';
+                end if;                       
             end if;
         end if;
+        COMPLETE <= complete_i;
+        complete_i := '0';
     end process;
-    
-    output: process (complete)
-    begin
-        if complete = '1' and password(0) /= 0 then
-            COMPLETE <= complete;
-            complete <= '0';
-        elsif complete = '1' and password(0) /= 0 and answer(0) /= 0 then
-            RESULT1 <= results(3);
-            RESULT2 <= results(2); 
-            RESULT3 <= results(1); 
-            RESULT4 <= results(0); 
-            CORRECT <= correct;
-            COMPLETE <= complete;
-            complete <= '0';
-            correct <= '1';
-            results <= (others => (others => '0'));
-        end if;
-    end process;   
 end Behavioral;
--- VHDL/vhdl_nexus_ddr4/Trabajo/Trabajo.srcs/sources_1/new/FSM.vhd
+++ VHDL/vhdl_nexus_ddr4/Trabajo/Trabajo.srcs/sources_1/new/FSM.vhd
@@ -3,29 +3,33 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity FSM is
+   	generic(
+    INPUT_WIDTH: positive := 2;       -- Number of input's elements
+    OUTPUT_WIDTH: positive := 3       -- Number of elements needed for output
+    );
     port (
     RESET :         in std_logic;
     CLK :           in std_logic;
     CREATEBUTTON:   in std_logic;   --Button for creation mode
-    BUTTONPRESSED:  in integer;     --Buttons pressed during game
+    BUTTONPRESSED:  in std_logic_vector (INPUT_WIDTH downto 0);     --Buttons pressed during game
     -- Comparator
     COMPLETE:       in std_logic;   --Full answer given
     CORRECT:        in std_logic;   --Correct answer given      
-    RESULT1 :       in std_logic_vector (3 downto 0);   --Comparator results
-    RESULT2 :       in std_logic_vector (3 downto 0);
-    RESULT3 :       in std_logic_vector (3 downto 0);
-    RESULT4 :       in std_logic_vector (3 downto 0);
-    PASSWORD :      out integer;    --Signal to define password
-    ANSWER :        out integer;    --Signal to define answer
+    RESULT1 :       in std_logic_vector (OUTPUT_WIDTH downto 0);   -- Comparator results
+    RESULT2 :       in std_logic_vector (OUTPUT_WIDTH downto 0);
+    RESULT3 :       in std_logic_vector (OUTPUT_WIDTH downto 0);
+    RESULT4 :       in std_logic_vector (OUTPUT_WIDTH downto 0);
+    PASSWORD :      out std_logic_vector (INPUT_WIDTH downto 0);    --Signal to define password
+    ANSWER :        out std_logic_vector (INPUT_WIDTH downto 0);    --Signal to define answer
     -- Display     
-    DISPLAY1 :      out std_logic_vector(3 downto 0);   --Display orders
-    DISPLAY2 :      out std_logic_vector(3 downto 0);
-    DISPLAY3 :      out std_logic_vector(3 downto 0);
-    DISPLAY4 :      out std_logic_vector(3 downto 0);
-    DISPLAY5 :      out std_logic_vector(3 downto 0);
-    DISPLAY6 :      out std_logic_vector(3 downto 0);
-    DISPLAY7 :      out std_logic_vector(3 downto 0);
-    DISPLAY8 :      out std_logic_vector(3 downto 0)   
+    DISPLAY1 :      out std_logic_vector(OUTPUT_WIDTH downto 0);   --Display orders
+    DISPLAY2 :      out std_logic_vector(OUTPUT_WIDTH downto 0);
+    DISPLAY3 :      out std_logic_vector(OUTPUT_WIDTH downto 0);
+    DISPLAY4 :      out std_logic_vector(OUTPUT_WIDTH downto 0);
+    DISPLAY5 :      out std_logic_vector(OUTPUT_WIDTH downto 0);
+    DISPLAY6 :      out std_logic_vector(OUTPUT_WIDTH downto 0);
+    DISPLAY7 :      out std_logic_vector(OUTPUT_WIDTH downto 0);
+    DISPLAY8 :      out std_logic_vector(OUTPUT_WIDTH downto 0)   
     );
 end FSM;
 
@@ -38,16 +42,16 @@ begin
         begin
     	if RESET = '0' then
         	current_state <= Start;
-        elsif rising_edge(clk) then
+        elsif rising_edge(CLK) then
         	current_state <= next_state;
         end if;
     end process;
 
-    nextstate_decod: process (COMPLETE, CORRECT, current_state)
+    nextstate_decod: process (RESET, CREATEBUTTON, COMPLETE, CORRECT, current_state)
     begin
         next_state <= current_state;
         case current_state is
-            when START =>
+            when Start =>
                 if CREATEBUTTON = '1' then
                     next_state <= Create;
                 end if;
@@ -79,102 +83,110 @@ begin
                 elsif COMPLETE = '1' and CORRECT = '1' then
                     next_state <= Victory;
                 end if;
+            when Victory =>
+                if RESET = '0' then
+                    next_state <= Start;
+                end if;
+            when Lose =>
+                if RESET = '0' then
+                    next_state <= Start;
+                end if;             
 
             when others =>
                 next_state <= Start;
         end case;
     end process;
     
-    output_decod: process (current_state)
+    output_decod: process (current_state, BUTTONPRESSED, RESULT1)
     begin
-        DISPLAY1  <= (OTHERS => '0');
-        DISPLAY2  <= (OTHERS => '0');
-        DISPLAY3  <= (OTHERS => '0');
-        DISPLAY4  <= (OTHERS => '0');
-        DISPLAY5  <= (OTHERS => '0');
-        DISPLAY6  <= (OTHERS => '0');
-        DISPLAY7  <= (OTHERS => '0');
-        DISPLAY8  <= (OTHERS => '0');
-        PASSWORD  <= 0;
-        ANSWER    <= 0;
+--        DISPLAY1  <= (OTHERS => '0');
+--        DISPLAY2  <= (OTHERS => '0');
+--        DISPLAY3  <= (OTHERS => '0');
+--        DISPLAY4  <= (OTHERS => '0');
+--        DISPLAY5  <= (OTHERS => '0');
+--        DISPLAY6  <= (OTHERS => '0');
+--        DISPLAY7  <= (OTHERS => '0');
+--        DISPLAY8  <= (OTHERS => '0');
+        PASSWORD  <= "000";
+        ANSWER    <= "000";
         case current_state is
             when Start  =>
-                DISPLAY1 <= "1010";
-                DISPLAY2 <= "1011";
-                DISPLAY3 <= "1100";
-                DISPLAY4 <= "1101";
-                DISPLAY5 <= "1101";
-                DISPLAY6 <= "1001";
-                DISPLAY7 <= "0000";
-                DISPLAY8 <= "0000";
+                DISPLAY1 <= "1010"; --g
+                DISPLAY2 <= "1011"; --u
+                DISPLAY3 <= "1100"; --e
+                DISPLAY4 <= "1101"; --s
+                DISPLAY5 <= "1101"; --s
+                DISPLAY6 <= "1001"; --9
+                DISPLAY7 <= "0000"; --0
+                DISPLAY8 <= "0000"; --0
             when Create =>
-                DISPLAY1 <= "1111";
-                DISPLAY2 <= "1111";
-                DISPLAY3 <= "1111";
-                DISPLAY4 <= "1111";
-                DISPLAY5 <= "1111";
-                DISPLAY6 <= "1111";
-                DISPLAY7 <= "1111";
-                DISPLAY8 <= "1111";
+                DISPLAY1 <= "1111"; ---
+                DISPLAY2 <= "1111"; ---
+                DISPLAY3 <= "1111"; ---
+                DISPLAY4 <= "1111"; ---
+                DISPLAY5 <= "1111"; ---
+                DISPLAY6 <= "1111"; ---
+                DISPLAY7 <= "1111"; ---
+                DISPLAY8 <= "1111"; ---
                 PASSWORD <= BUTTONPRESSED;
             when S3 =>
-                DISPLAY1 <= "1110";
-                DISPLAY2 <= "1110";
-                DISPLAY3 <= "1110";
-                DISPLAY4 <= "1110";
+                DISPLAY1 <= "1110"; --º
+                DISPLAY2 <= "1110"; --º
+                DISPLAY3 <= "1110"; --º
+                DISPLAY4 <= "1110"; --º
                 DISPLAY5 <= RESULT1;
                 DISPLAY6 <= RESULT2;
                 DISPLAY7 <= RESULT3;
                 DISPLAY8 <= RESULT4;
                 ANSWER   <= BUTTONPRESSED;
             when S2 =>
-                DISPLAY1 <= "1110";
-                DISPLAY2 <= "1110";
-                DISPLAY3 <= "1110";
-                DISPLAY4 <= "1111";
+                DISPLAY1 <= "1110"; --º
+                DISPLAY2 <= "1110"; --º
+                DISPLAY3 <= "1110"; --º
+                DISPLAY4 <= "1111"; ---
                 DISPLAY5 <= RESULT1;
                 DISPLAY6 <= RESULT2;
                 DISPLAY7 <= RESULT3;
                 DISPLAY8 <= RESULT4;
                 ANSWER   <= BUTTONPRESSED;
             when S1 =>
-                DISPLAY1 <= "1110";
-                DISPLAY2 <= "1110";
-                DISPLAY3 <= "1111";
-                DISPLAY4 <= "1111";
+                DISPLAY1 <= "1110"; --º
+                DISPLAY2 <= "1110"; --º
+                DISPLAY3 <= "1111"; ---
+                DISPLAY4 <= "1111"; ---
                 DISPLAY5 <= RESULT1;
                 DISPLAY6 <= RESULT2;
                 DISPLAY7 <= RESULT3;
                 DISPLAY8 <= RESULT4;
                 ANSWER   <= BUTTONPRESSED;
             when S0 =>
-                DISPLAY1 <= "1110";
-                DISPLAY2 <= "1111";
-                DISPLAY3 <= "1111";
-                DISPLAY4 <= "1111";
+                DISPLAY1 <= "1110"; --º
+                DISPLAY2 <= "1111"; ---
+                DISPLAY3 <= "1111"; ---
+                DISPLAY4 <= "1111"; ---
                 DISPLAY5 <= RESULT1;
                 DISPLAY6 <= RESULT2;
                 DISPLAY7 <= RESULT3;
                 DISPLAY8 <= RESULT4;
                 ANSWER   <= BUTTONPRESSED;
             when Victory   =>
-                DISPLAY1 <= "1000";
-                DISPLAY2 <= "1000";
-                DISPLAY3 <= "1000";
-                DISPLAY4 <= "1000";
-                DISPLAY5 <= "1000";
-                DISPLAY6 <= "1000";
-                DISPLAY7 <= "1000";
-                DISPLAY8 <= "1000";
+                DISPLAY1 <= "1000"; --8
+                DISPLAY2 <= "1000"; --8
+                DISPLAY3 <= "1000"; --8
+                DISPLAY4 <= "1000"; --8
+                DISPLAY5 <= "1000"; --8
+                DISPLAY6 <= "1000"; --8
+                DISPLAY7 <= "1000"; --8
+                DISPLAY8 <= "1000"; --8
             when Lose    =>
-                DISPLAY1 <= "1111";
-                DISPLAY2 <= "1111";
-                DISPLAY3 <= "1111";
-                DISPLAY4 <= "1111";
-                DISPLAY5 <= "1111";
-                DISPLAY6 <= "1111";
-                DISPLAY7 <= "1111";
-                DISPLAY8 <= "1111";
+                DISPLAY1 <= "1111"; ---
+                DISPLAY2 <= "1111"; ---
+                DISPLAY3 <= "1111"; ---
+                DISPLAY4 <= "1111"; ---
+                DISPLAY5 <= "1111"; ---
+                DISPLAY6 <= "1111"; ---
+                DISPLAY7 <= "1111"; ---
+                DISPLAY8 <= "1111"; ---
             when others =>
                 DISPLAY1  <= (OTHERS => '0');
                 DISPLAY2  <= (OTHERS => '0');
@@ -184,8 +196,8 @@ begin
                 DISPLAY6  <= (OTHERS => '0');
                 DISPLAY7  <= (OTHERS => '0');
                 DISPLAY8  <= (OTHERS => '0');
-                PASSWORD  <= 0;
-                ANSWER    <= 0;
+                PASSWORD  <= "000";
+                ANSWER    <= "000";
         end case;
     end process;
 end behavioral;
