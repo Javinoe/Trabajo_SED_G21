cdda36f547628b8dd4c7d3a293205f416f5b53b6
Javinoe <99390938+Javinoe@users.noreply.github.com>
2022-12-22 19:26:04 +0100

    update encoder v2.0
    Update Encoder 2.0

--- VHDL/vhdl_nexus_ddr4/Trabajo/Trabajo.srcs/sources_1/new/Encoder.vhd
+++ VHDL/vhdl_nexus_ddr4/Trabajo/Trabajo.srcs/sources_1/new/Encoder.vhd
@@ -3,39 +3,9 @@ use IEEE.std_logic_1164.all;
 
 entity Encoder is
     port (
-     V_BINARIO : out std_logic_vector(3 downto 0);
-     x4, x3, x2, x1, x0 : in std_logic
-    );
-end entity;
-
-architecture Encoder of Encoder is
-signal vbinariox : std_logic_vector (3 downto 0);
-begin
-
-    process ( x4, x3, x2, x1, x0) 
-
-begin 
-if    x0='1' then vbinariox <= "0001" ;
-elsif x1='1' then vbinariox <= "0010" ;   
-elsif x2='1' then vbinariox <= "0011" ; 
-elsif x3='1' then vbinariox <= "0100" ; 
-elsif x4='1' then vbinariox <= "0101" ; 
-        else vbinariox <= "0000";
-        end if;
-
-    end process;
-    V_BINARIO <= vbinariox;
-end Encoder;
-
-    library IEEE;
-use IEEE.std_logic_1164.all;
-
-entity Encoder is
-    port (
      buttonpressed : out std_logic_vector(3 downto 0);
      createbutton :  out std_logic; 
-     x4, x3, x2, x1, x0 : in std_logic;
- 
+     x4, x3, x2, x1, x0 : in std_logic
     );
 end entity;
 
