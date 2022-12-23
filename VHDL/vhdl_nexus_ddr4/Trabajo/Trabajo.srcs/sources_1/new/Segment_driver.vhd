6ed44f491b45634a0152cf8ad6d9df78f6b67286
Javinoe <99390938+Javinoe@users.noreply.github.com>
2022-12-23 17:00:37 +0100

    Update Segment_driver.vhd
    mayus fix

--- VHDL/vhdl_nexus_ddr4/Trabajo/Trabajo.srcs/sources_1/new/Segment_driver.vhd
+++ VHDL/vhdl_nexus_ddr4/Trabajo/Trabajo.srcs/sources_1/new/Segment_driver.vhd
@@ -3,56 +3,56 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
---display_A->display_D respuestas usuario
---display_E->display_H contador de vidas
+--DISPLAY_A->DISPLAY_D respuestas usuario
+--DISPLAY_E->DISPLAY_H contador de vidas
 
 entity segmentdriver is
-Port( display_A : in STD_LOGIC_VECTOR (3 downto 0);
-    display_B : in STD_LOGIC_VECTOR (3 downto 0);
-    display_C : in STD_LOGIC_VECTOR (3 downto 0);
-    display_D : in STD_LOGIC_VECTOR (3 downto 0);
-    display_E : in STD_LOGIC_VECTOR (3 downto 0);
-    display_F : in STD_LOGIC_VECTOR (3 downto 0);
-    display_G : in STD_LOGIC_VECTOR (3 downto 0);
-    display_H : in STD_LOGIC_VECTOR (3 downto 0);
-    seg_A : out STD_LOGIC;
-    seg_B : out STD_LOGIC;
-    seg_C : out STD_LOGIC;
-    seg_D : out STD_LOGIC;
-    seg_E : out STD_LOGIC;
-    seg_F : out STD_LOGIC;
-    seg_G : out STD_LOGIC;
-    select_display_A : out STD_LOGIC;
-    select_display_B : out STD_LOGIC;
-    select_display_C : out STD_LOGIC;
-    select_display_D : out STD_LOGIC;
-    select_display_E : out STD_LOGIC;
-    select_display_F : out STD_LOGIC;
-    select_display_G : out STD_LOGIC;
-    select_display_H : out STD_LOGIC;
-    clk : in STD_LOGIC);
+Port( DISPLAY_A : in STD_LOGIC_VECTOR (3 downto 0);
+    DISPLAY_B : in STD_LOGIC_VECTOR (3 downto 0);
+    DISPLAY_C : in STD_LOGIC_VECTOR (3 downto 0);
+    DISPLAY_D : in STD_LOGIC_VECTOR (3 downto 0);
+    DISPLAY_E : in STD_LOGIC_VECTOR (3 downto 0);
+    DISPLAY_F : in STD_LOGIC_VECTOR (3 downto 0);
+    DISPLAY_G : in STD_LOGIC_VECTOR (3 downto 0);
+    DISPLAY_H : in STD_LOGIC_VECTOR (3 downto 0);
+    SEG_A : out STD_LOGIC;
+    SEG_B : out STD_LOGIC;
+    SEG_C : out STD_LOGIC;
+    SEG_D : out STD_LOGIC;
+    SEG_E : out STD_LOGIC;
+    SEG_F : out STD_LOGIC;
+    SEG_G : out STD_LOGIC;
+    SELECT_DISPLAY_A : out STD_LOGIC;
+    SELECT_DISPLAY_B : out STD_LOGIC;
+    SELECT_DISPLAY_C : out STD_LOGIC;
+    SELECT_DISPLAY_D : out STD_LOGIC;
+    SELECT_DISPLAY_E : out STD_LOGIC;
+    SELECT_DISPLAY_F : out STD_LOGIC;
+    SELECT_DISPLAY_G : out STD_LOGIC;
+    SELECT_DISPLAY_H : out STD_LOGIC;
+    CLK : in STD_LOGIC);
 end segmentdriver;
 
 architecture Behavioral of segmentdriver is
 --Component Declaration
 
 COMPONENT segmentdecoder is
-Port ( Value : in STD_LOGIC_VECTOR (3 downto 0);
-    segment_A: out STD_LOGIC;
-    segment_B: out STD_LOGIC;
-    segment_C: out STD_LOGIC;
-    segment_D: out STD_LOGIC;
-    segment_E: out STD_LOGIC;
-    segment_F: out STD_LOGIC;
-    segment_G: out STD_LOGIC);
+Port ( VALUE : in STD_LOGIC_VECTOR (3 downto 0);
+    SEGMENT_A: out STD_LOGIC;
+    SEGMENT_B: out STD_LOGIC;
+    SEGMENT_C: out STD_LOGIC;
+    SEGMENT_D: out STD_LOGIC;
+    SEGMENT_E: out STD_LOGIC;
+    SEGMENT_F: out STD_LOGIC;
+    SEGMENT_G: out STD_LOGIC);
 END COMPONENT;
 
 COMPONENT clk_divider is
     PORT(
-    clk : in STD_LOGIC;
-    enable : in STD_LOGIC;
-    reset : in STD_LOGIC;
-    data_clk : out STD_LOGIC_VECTOR (15 downto 0));
+    CLK : in STD_LOGIC;
+    ENABLE : in STD_LOGIC;
+    RESET : in STD_LOGIC;
+    DATA_CLK : out STD_LOGIC_VECTOR (15 downto 0));
 END COMPONENT;
     
 SIGNAL aux_data : std_logic_vector(3 downto 0);
@@ -62,147 +62,147 @@ SIGNAL slow_clock : std_logic;
 begin
 -- Component Instantiation
 uut: segmentdecoder PORT MAP(
-        Value => aux_data,
-        segment_A => seg_A,
-        segment_B => seg_B,
-        segment_C => seg_C,
-        segment_D => seg_D,
-        segment_E => seg_E,
-        segment_F => seg_F,
-        segment_G => seg_G);
+        VALUE => aux_data,
+        SEGMENT_A => SEG_A,
+        SEGMENT_B => SEG_B,
+        SEGMENT_C => SEG_C,
+        SEGMENT_D => SEG_D,
+        SEGMENT_E => SEG_E,
+        SEGMENT_F => SEG_F,
+        SEGMENT_G => SEG_G);
         
 uut1: clk_divider PORT MAP(
-        clk => clk,
-        enable => '1',
-        reset => '0',
-        data_clk => clock_word);
+        CLK => CLK,
+        ENABLE => '1',
+        RESET => '0',
+        DATA_CLK => clock_word);
         
 slow_clock <= clock_word(0); --Divisor del reloj, a 0 es un calco del mismo
 
 PROCESS (slow_clock)
-variable display_selection : STD_LOGIC_VECTOR(2 downto 0);
+variable DISPLAY_SELECTion : STD_LOGIC_VECTOR(2 downto 0);
 BEGIN
 
 
 if slow_clock'event and slow_clock = '1' then
 
-case display_selection is
+case DISPLAY_SELECTion is
 
-when "000" => aux_data <= display_A;
+when "000" => aux_data <= DISPLAY_A;
 
-    select_display_A <= '0'; 
-    select_display_B <= '1';
-    select_display_C <= '1';
-    select_display_D <= '1';
-    select_display_E <= '1';
-    select_display_F <= '1';
-    select_display_G <= '1';
-    select_display_H <= '1';
+    SELECT_DISPLAY_A <= '0'; 
+    SELECT_DISPLAY_B <= '1';
+    SELECT_DISPLAY_C <= '1';
+    SELECT_DISPLAY_D <= '1';
+    SELECT_DISPLAY_E <= '1';
+    SELECT_DISPLAY_F <= '1';
+    SELECT_DISPLAY_G <= '1';
+    SELECT_DISPLAY_H <= '1';
     
-    display_selection := display_selection+'1';
+    DISPLAY_SELECTion := DISPLAY_SELECTion+'1';
 
-when "001" => aux_data <= display_B;
+when "001" => aux_data <= DISPLAY_B;
 
-    select_display_A <= '1'; 
-    select_display_B <= '0';
-    select_display_C <= '1';
-    select_display_D <= '1';
-    select_display_E <= '1';
-    select_display_F <= '1';
-    select_display_G <= '1';
-    select_display_H <= '1';
+    SELECT_DISPLAY_A <= '1'; 
+    SELECT_DISPLAY_B <= '0';
+    SELECT_DISPLAY_C <= '1';
+    SELECT_DISPLAY_D <= '1';
+    SELECT_DISPLAY_E <= '1';
+    SELECT_DISPLAY_F <= '1';
+    SELECT_DISPLAY_G <= '1';
+    SELECT_DISPLAY_H <= '1';
     
-    display_selection := display_selection+'1';
+    DISPLAY_SELECTion := DISPLAY_SELECTion+'1';
 
-when "010" => aux_data <= display_C;
+when "010" => aux_data <= DISPLAY_C;
 
-    select_display_A <= '1'; 
-    select_display_B <= '1';
-    select_display_C <= '0';
-    select_display_D <= '1';
-    select_display_E <= '1';
-    select_display_F <= '1';
-    select_display_G <= '1';
-    select_display_H <= '1';
+    SELECT_DISPLAY_A <= '1'; 
+    SELECT_DISPLAY_B <= '1';
+    SELECT_DISPLAY_C <= '0';
+    SELECT_DISPLAY_D <= '1';
+    SELECT_DISPLAY_E <= '1';
+    SELECT_DISPLAY_F <= '1';
+    SELECT_DISPLAY_G <= '1';
+    SELECT_DISPLAY_H <= '1';
     
-    display_selection := display_selection+'1';
+    DISPLAY_SELECTion := DISPLAY_SELECTion+'1';
 
-when "011" => aux_data <= display_D;
+when "011" => aux_data <= DISPLAY_D;
 
-    select_display_A <= '1'; 
-    select_display_B <= '1';
-    select_display_C <= '1';
-    select_display_D <= '0';
-    select_display_E <= '1';
-    select_display_F <= '1';
-    select_display_G <= '1';
-    select_display_H <= '1';
+    SELECT_DISPLAY_A <= '1'; 
+    SELECT_DISPLAY_B <= '1';
+    SELECT_DISPLAY_C <= '1';
+    SELECT_DISPLAY_D <= '0';
+    SELECT_DISPLAY_E <= '1';
+    SELECT_DISPLAY_F <= '1';
+    SELECT_DISPLAY_G <= '1';
+    SELECT_DISPLAY_H <= '1';
     
-    display_selection := display_selection+'1';
+    DISPLAY_SELECTion := DISPLAY_SELECTion+'1';
 
-when "100" => aux_data <= display_E;
+when "100" => aux_data <= DISPLAY_E;
 
-    select_display_A <= '1'; 
-    select_display_B <= '1';
-    select_display_C <= '1';
-    select_display_D <= '1';
-    select_display_E <= '0';
-    select_display_F <= '1';
-    select_display_G <= '1';
-    select_display_H <= '1';
+    SELECT_DISPLAY_A <= '1'; 
+    SELECT_DISPLAY_B <= '1';
+    SELECT_DISPLAY_C <= '1';
+    SELECT_DISPLAY_D <= '1';
+    SELECT_DISPLAY_E <= '0';
+    SELECT_DISPLAY_F <= '1';
+    SELECT_DISPLAY_G <= '1';
+    SELECT_DISPLAY_H <= '1';
     
-    display_selection := display_selection+'1';
+    DISPLAY_SELECTion := DISPLAY_SELECTion+'1';
 
-when "101" => aux_data <= display_F;
+when "101" => aux_data <= DISPLAY_F;
 
-    select_display_A <= '1'; 
-    select_display_B <= '1';
-    select_display_C <= '1';
-    select_display_D <= '1';
-    select_display_E <= '1';
-    select_display_F <= '0';
-    select_display_G <= '1';
-    select_display_H <= '1';
+    SELECT_DISPLAY_A <= '1'; 
+    SELECT_DISPLAY_B <= '1';
+    SELECT_DISPLAY_C <= '1';
+    SELECT_DISPLAY_D <= '1';
+    SELECT_DISPLAY_E <= '1';
+    SELECT_DISPLAY_F <= '0';
+    SELECT_DISPLAY_G <= '1';
+    SELECT_DISPLAY_H <= '1';
     
-    display_selection := display_selection+'1';
+    DISPLAY_SELECTion := DISPLAY_SELECTion+'1';
 
-when "110" => aux_data <= display_G;
+when "110" => aux_data <= DISPLAY_G;
 
-    select_display_A <= '1'; 
-    select_display_B <= '1';
-    select_display_C <= '1';
-    select_display_D <= '1';
-    select_display_E <= '1';
-    select_display_F <= '1';
-    select_display_G <= '0';
-    select_display_H <= '1';
+    SELECT_DISPLAY_A <= '1'; 
+    SELECT_DISPLAY_B <= '1';
+    SELECT_DISPLAY_C <= '1';
+    SELECT_DISPLAY_D <= '1';
+    SELECT_DISPLAY_E <= '1';
+    SELECT_DISPLAY_F <= '1';
+    SELECT_DISPLAY_G <= '0';
+    SELECT_DISPLAY_H <= '1';
     
-    display_selection := display_selection+'1';
+    DISPLAY_SELECTion := DISPLAY_SELECTion+'1';
 
-when "111" => aux_data <= display_H;
+when "111" => aux_data <= DISPLAY_H;
 
-    select_display_A <= '1'; 
-    select_display_B <= '1';
-    select_display_C <= '1';
-    select_display_D <= '1';
-    select_display_E <= '1';
-    select_display_F <= '1';
-    select_display_G <= '1';
-    select_display_H <= '0';
+    SELECT_DISPLAY_A <= '1'; 
+    SELECT_DISPLAY_B <= '1';
+    SELECT_DISPLAY_C <= '1';
+    SELECT_DISPLAY_D <= '1';
+    SELECT_DISPLAY_E <= '1';
+    SELECT_DISPLAY_F <= '1';
+    SELECT_DISPLAY_G <= '1';
+    SELECT_DISPLAY_H <= '0';
     
-    display_selection := "000";
+    DISPLAY_SELECTion := "000";
     
 when others =>
 
-    select_display_A <= '1'; 
-    select_display_B <= '1';
-    select_display_C <= '1';
-    select_display_D <= '1';
-    select_display_E <= '1';
-    select_display_F <= '1';
-    select_display_G <= '1';
-    select_display_H <= '1';
-    display_selection := "000";
+    SELECT_DISPLAY_A <= '1'; 
+    SELECT_DISPLAY_B <= '1';
+    SELECT_DISPLAY_C <= '1';
+    SELECT_DISPLAY_D <= '1';
+    SELECT_DISPLAY_E <= '1';
+    SELECT_DISPLAY_F <= '1';
+    SELECT_DISPLAY_G <= '1';
+    SELECT_DISPLAY_H <= '1';
+    DISPLAY_SELECTion := "000";
 
 end case;
 end if;
