ab0743f87f54a498510ea1584e77b8e5f40686c0
Javinoe <99390938+Javinoe@users.noreply.github.com>
2022-12-23 16:57:26 +0100

    Update clk_divider.vhd
    fix mayus port

--- VHDL/vhdl_nexus_ddr4/Trabajo/Trabajo.srcs/sources_1/new/clk_divider.vhd
+++ VHDL/vhdl_nexus_ddr4/Trabajo/Trabajo.srcs/sources_1/new/clk_divider.vhd
@@ -1,43 +1,13 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 12/15/2022 09:27:51 PM
--- Design Name: 
--- Module Name: clk_divider - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity clk_divider is
-    Port ( clk : in STD_LOGIC;
-           enable : in STD_LOGIC;
-           reset : in STD_LOGIC;
-           data_clk : out STD_LOGIC_VECTOR (15 downto 0));
+    Port ( CLK : in STD_LOGIC;
+           ENABLE : in STD_LOGIC;
+           RESET : in STD_LOGIC;
+           DATA_CLK : out STD_LOGIC_VECTOR (15 downto 0));
 end clk_divider;
 
 architecture Behavioral of clk_divider is
@@ -55,4 +25,5 @@ begin
     data_clk <= count;
     end process;
 
+end Behavioral;
 end Behavioral;
